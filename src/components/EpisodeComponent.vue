0d60d0bd6a01092651ee5f7464883a09f927a7d4
Simeon Petrov <s.petrov@blubito.com>
2021-02-09 15:59:44 +0200

    part02/favourite-episodes

--- src/components/EpisodeComponent.vue
+++ src/components/EpisodeComponent.vue
@@ -3,11 +3,35 @@
     <template #header>
       <div class="clearfix">
         <span>{{ episode.episode }} - {{ episode.name }}</span>
+        <img
+          v-if="isFavourite"
+          class="favIcon"
+          src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pjxzdmcgdmlld0JveD0iMCAwIDMyIDMyIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxkZWZzPjxzdHlsZT4uY2xzLTF7ZmlsbDojMTAxODIwO308L3N0eWxlPjwvZGVmcz48dGl0bGUvPjxnIGRhdGEtbmFtZT0iTGF5ZXIgNTQiIGlkPSJMYXllcl81NCI+PHBhdGggY2xhc3M9ImNscy0xIiBkPSJNMTYsMjguNzJhMywzLDAsMCwxLTIuMTMtLjg4TDMuNTcsMTcuNTRhOC43Miw4LjcyLDAsMCwxLTIuNTItNi4yNSw4LjA2LDguMDYsMCwwLDEsOC4xNC04QTguMDYsOC4wNiwwLDAsMSwxNSw1LjY4bDEsMSwuODItLjgyaDBhOC4zOSw4LjM5LDAsMCwxLDExLS44OSw4LjI1LDguMjUsMCwwLDEsLjgxLDEyLjM2TDE4LjEzLDI3Ljg0QTMsMywwLDAsMSwxNiwyOC43MlpNOS4xNSw1LjI4QTYuMTIsNi4xMiwwLDAsMCw0Ljg5LDdhNiw2LDAsMCwwLTEuODQsNC4zM0E2LjcyLDYuNzIsMCwwLDAsNSwxNi4xM2wxMC4zLDEwLjNhMSwxLDAsMCwwLDEuNDIsMEwyNy4yMywxNS45MUE2LjI1LDYuMjUsMCwwLDAsMjksMTEuMTFhNi4xOCw2LjE4LDAsMCwwLTIuNDMtNC41NSw2LjM3LDYuMzcsMCwwLDAtOC4zNy43MUwxNi43MSw4LjhhMSwxLDAsMCwxLTEuNDIsMGwtMS43LTEuN2E2LjI4LDYuMjgsMCwwLDAtNC40LTEuODJaIi8+PC9nPjwvc3ZnPg=="
+        />
       </div>
     </template>
     <div>
       <el-tag class="customTag">AIR: {{ episode.air_date }}</el-tag>
-      <el-tag class="customTag">CREATED: {{ formatDate(episode.created) }}</el-tag>
+      <el-tag class="customTag"
+        >CREATED: {{ formatDate(episode.created) }}</el-tag
+      >
+    </div>
+    <div class="actions">
+      <el-button
+        type="primary"
+        icon="el-icon-star-off"
+        circle
+        @click="addToFavourites"
+        :disabled="isLimitPassed || isFavourite"
+      />
+
+      <el-button
+        type="danger"
+        icon="el-icon-delete"
+        circle
+        @click="removeFromFavourites"
+        :disabled="!isFavourite"
+      />
     </div>
   </el-card>
 </template>
@@ -16,19 +40,46 @@
 export default {
   props: {
     episode: Object,
+    isFavourite: Boolean,
+    isLimitPassed: Boolean,
   },
-  methods:{
-      formatDate(originalDate){
-          const date = new Date(originalDate);
-          return new Intl.DateTimeFormat('en-GB').format(date);
-      }
-  }
+  methods: {
+    formatDate(originalDate) {
+      const date = new Date(originalDate);
+      return new Intl.DateTimeFormat("en-GB").format(date);
+    },
+    addToFavourites() {
+      this.$emit("add-to-fav", this.episode.id);
+    },
+    removeFromFavourites() {
+      this.$emit("remove-from-fav", this.episode.id);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.box-card {
+  position: relative;
+  overflow: hidden;
+}
 .customTag:not(:last-child) {
   margin-right: 1rem;
   margin-bottom: 0.25rem;
+}
+.actions {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: flex-end;
+}
+.favIcon {
+  margin-left: 1rem;
+  width: 2rem;
+  background-color: rgba(#67c23a, 0.7);
+  padding: 6px;
+  border-bottom-left-radius: 10px;
+  position: absolute;
+  right: 0;
+  top: 0;
 }
 </style>
--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -2,9 +2,19 @@
 <template>
   <div v-loading="isLoading" class="page">
     <h1>Rick and Moprty Episodes</h1>
+
+    <el-tag class="favTag" :type="tagType">
+      Favourites: {{ favourites.length }} / {{ maxNumberFavourites }}
+    </el-tag>
     <ul class="customGrid">
       <li v-for="episode in episodes" :key="episode.id">
-        <EpisodeComponent :episode="episode" />
+        <EpisodeComponent
+          :episode="episode"
+          :is-favourite="isInFavourites(episode.id)"
+          :is-limit-passed="checkFavouriteLimit"
+          @add-to-fav="addToFavouriteList"
+          @remove-from-fav="removeFromFav"
+        />
       </li>
     </ul>
   </div>
@@ -15,6 +25,9 @@ import Episode from "@/types/Episode";
 import { defineComponent } from "vue";
 import EpisodeComponent from "../components/EpisodeComponent.vue";
 import { getAllEpisodes } from "../dataProviders/dataProvider";
+
+const warningMsg = 'Warning, you have $val more favourite slots left!';
+
 export default defineComponent({
   components: {
     EpisodeComponent,
@@ -23,7 +36,49 @@ export default defineComponent({
     return {
       isLoading: true,
       episodes: [] as Episode[],
+      favourites: [] as number[],
+      maxNumberFavourites: 10,
+      tagType: "success",
     };
+  },
+  computed: {
+    checkFavouriteLimit(): boolean {
+      return this.favourites.length >= this.maxNumberFavourites;
+    },
+  },
+  watch: {
+    favourites: {
+      deep: true,
+      handler(newData) {
+        const currentLength = newData.length;
+        switch (currentLength) {
+          case 5:
+            window.alert(warningMsg.replace('$val', '5'));
+            break;
+          case 8:
+            window.alert(warningMsg.replace('$val', '2'));
+            this.tagType = "warning";
+            break;
+          case 9:
+            window.alert(warningMsg.replace('$val', '1'));
+            break;
+          case 10:
+            this.tagType = "danger";
+            break;
+        }
+      },
+    },
+  },
+  methods: {
+    addToFavouriteList(id: number): void {
+      this.favourites.push(id);
+    },
+    isInFavourites(elemtId: number): boolean {
+      return this.favourites.includes(elemtId);
+    },
+    removeFromFav(id: number): void {
+      this.favourites = this.favourites.filter((favId) => favId !== id);
+    },
   },
   async created() {
     const { results } = await getAllEpisodes();
@@ -33,7 +88,13 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
+.favTag {
+  display: block !important;
+  width: 130px;
+  margin-left: auto;
+  margin-bottom: 2rem;
+}
 .customGrid {
   display: grid;
   grid-template-columns: repeat(5, minmax(250px, 1fr));
